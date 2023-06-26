9966d03d7ec4caeb09be93e553f9d9a467bce180
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2023-06-26 13:53:05 +0800

    macros[1,2,3]

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -3,14 +3,18 @@
 // Execute `rustlings hint macros2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+use wwj::my_macro;
 
 fn main() {
     my_macro!();
 }
 
-macro_rules! my_macro {
+mod wwj {
+    macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    };
+        };
+    }
+    pub(crate) use my_macro;
 }
